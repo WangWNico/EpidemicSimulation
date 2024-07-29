9e1c1c2b2ecf27ac8f4e2ce2de6db3625bdb03e8
WangWNico <nicowang0404@gmail.com>
2024-07-29 14:31:15 -0400

    Created Project Structure

---
+++ src/main/java/edu/rpi/cs/csci4963/u24/rcsid/hw02/gol_gui/graph/HelloController.java
@@ -0,0 +1,14 @@
+package edu.rpi.cs.csci4963.u24.rcsid.hw02.gol_gui.graph;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.rpi.cs.csci4963.u24.rcsid.hw02.gol_gui.graph {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.rpi.cs.csci4963.u24.rcsid.hw02.gol_gui.graph to javafx.fxml;
+    exports edu.rpi.cs.csci4963.u24.rcsid.hw02.gol_gui.graph;
+}
